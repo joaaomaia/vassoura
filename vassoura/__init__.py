f12dbfc81c89b599b59732f77cf83af3e3c2eea6
maiaufrrj <52588085+maiaufrrj@users.noreply.github.com>
2025-06-04 23:36:43 -0300

    exemplos alterados

--- vassoura/__init__.py
+++ vassoura/__init__.py
@@ -1,11 +1,10 @@
-# vassoura/__init__.py
-
 from .utils import search_dtypes, suggest_corr_method, figsize_from_matrix
 from .correlacao import compute_corr_matrix, plot_corr_heatmap
 from .vif import compute_vif, remove_high_vif
 from .limpeza import clean
 from .relatorio import generate_report
 from .autocorrelacao import compute_panel_acf, plot_panel_acf
+from .analisador import analisar_autocorrelacao
 
 __all__ = [
     "search_dtypes",
@@ -18,5 +17,6 @@ __all__ = [
     "clean",
     "generate_report",
     "compute_panel_acf",
-    "plot_panel_acf"
+    "plot_panel_acf",
+    "analisar_autocorrelacao"
 ]
